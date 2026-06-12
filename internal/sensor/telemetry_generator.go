3584e977c233c7fb37d80065086bc7acc068e333
21 <victorsouzam74@gmail.com>
2026-06-12 14:18:45 -0300

    refactor load generator

--- internal/sensor/telemetry_generator.go
+++ internal/sensor/telemetry_generator.go
@@ -2,34 +2,36 @@ package generator
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"math/rand"
 	"time"
 
-	"ormuz-ledger/internal/domain"
+	"strait-of-ormuz/internal/domain"
+
 	"github.com/google/uuid"
 )
 
 type Engine struct {
-	SensorID            string
-	SectorID            int
+	TotalSectors        int
 	Frequency           time.Duration
 	ThreatProbability   float64
 	CriticalProbability float64
+	Nations             []string // Lista de Nações do Consórcio
 }
 
-// Loop gerando dados e enviando pelo canal
+// Start inicia o loop gerando dados multiplexados e enviando pelo canal
 func (e *Engine) Start(ctx context.Context, out chan<- domain.Telemetry) {
-	
+
 	ticker := time.NewTicker(e.Frequency)
 	defer ticker.Stop()
-
-	//rand.Seed(time.Now().UnixNano()) - obsoleto para versões 1.20+
 
 	for {
 		select {
 		case <-ctx.Done():
-			log.Printf("[GERADOR %s] Desligando...", e.SensorID)
+			log.Printf("[GERADOR DE CARGA] Desligando motor multiplexado...")
 			return
 		case <-ticker.C:
 			e.fire(out)
@@ -41,18 +43,33 @@ func (e *Engine) fire(out chan<- domain.Telemetry) {
 	isThreat := rand.Float64() < e.ThreatProbability
 	isCritical := false
 	eventID := "N/A"
+	signature := ""
+
+	// 1. Multiplexação: Escolhe um setor aleatório para simular neste instante
+	sectorID := rand.Intn(e.TotalSectors) + 1
+	sensorID := fmt.Sprintf("sensor-sec-%02d", sectorID)
+
+	// 2. Simulação de Evento: Decide aleatoriamente se é um evento de ameaça ou não
+	nationIndex := rand.Intn(len(e.Nations))
+	nationID := e.Nations[nationIndex]
 
 	if isThreat {
 		eventID = uuid.New().String()
 		isCritical = rand.Float64() < e.CriticalProbability
+
+		// 3. Simulação de Assinatura Criptográfica
+		// O hash atrela a identidade da nação que está pagando pelo evento gerado.
+		rawSig := fmt.Sprintf("%s:%s:secret-key", eventID, nationID)
+		hash := sha256.Sum256([]byte(rawSig))
+		signature = hex.EncodeToString(hash[:])
 	}
 
-	payload := domain.NewTelemetry(eventID, e.SensorID, e.SectorID, isThreat, isCritical)
+	payload := domain.NewTelemetry(eventID, sensorID, sectorID, isThreat, isCritical, nationID, signature)
 
 	// Backpressure
 	select {
 	case out <- payload:
 	default:
-		//log.Printf("[GERADOR %s] Aviso: Canal cheio, descartando pacote", e.SensorID)
+		// Se o canal encher (brokers lentos), descartamos silenciosamente - Adicionar log de aviso se necessário
 	}
 }
